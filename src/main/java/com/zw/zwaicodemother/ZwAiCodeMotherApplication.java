e99e89699256b2b8878a7b0028cc5363e647137e
zw <2267982308@qq.com>
2025-07-29 09:50:01 +0800

    新项目创建

---
+++ src/main/java/com/zw/zwaicodemother/ZwAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.zw.zwaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwAiCodeMotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwAiCodeMotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zw/zwaicodemother/ZwAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw.zwaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwAiCodeMotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
