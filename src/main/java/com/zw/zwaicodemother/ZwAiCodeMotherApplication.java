6fd70a56d99635e34e82dff40923f126af652448
zw <2267982308@qq.com>
2025-07-29 09:50:22 +0800

    新项目创建

--- src/main/java/com/zw/zwaicodemother/ZwAiCodeMotherApplication.java
+++ src/main/java/com/zw/zwaicodemother/ZwAiCodeMotherApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZwAiCodeMotherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZwAiCodeMotherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZwAiCodeMotherApplication.class, args);
+    }
 
 }
